1e32be332b37edcaea1e41312955f24d7f313bf0
Ulisesjhm1510 <ulisesjhm@gmail.com>
2023-05-30 16:04:42 -0400

    cambios en la plantilla del correo para confirmar el pedido

--- apps/api/src/app/orders/classes/SenderOrderMail.class.ts
+++ apps/api/src/app/orders/classes/SenderOrderMail.class.ts
@@ -19,7 +19,7 @@ export class SenderOrderMail extends SenderOrder{
             port:465,
             secure: true,
             auth: {
-                user:'secretfriend202212@gmail.com',
+                user:'secretfriend202212@gmail.com',    
                 pass:"ekshkcoyaswjaley"
             }
         })
@@ -32,19 +32,98 @@ export class SenderOrderMail extends SenderOrder{
 
     private makeTemplate(orderData: Order): string{
         return `
-        <div style=" border-radius:0.45rem;  background-color: #F2E205; font-family: sans-serif;" class="mail-body">
-            <h1 style="padding:1rem; color:#021F59; font-family:sans-serif; border-bottom:3px solid #aaaa;">Nuevo Pedido</h1>
-            <div style="padding:0 1rem;">
-                <p style="font-size:1rem; font-weight:500;">Cliente: ${orderData.nombre}</p>
-                <p style="font-size:1rem; font-weight:500;">Cedula: ${orderData.cedula}</p>
-                <p style="font-size:1rem; font-weight:500;">Correo Electronico: ${orderData.correo_electronico}</p>
-                <p style="font-size:1rem; font-weight:500;">Pedido: ${orderData.pedido}</p>
-                <p style="font-size:1rem; font-weight:500;">Direccion: ${orderData.direccion}</p>
+        <style>
+            *{
+                margin:0;
+                padding:0;
+            }
+            html{
+                font-family: sans-serif;
+            }
+            body{
+                margin: 0.5rem;
+
+            }
+            .card{
+                width: 400px;
+                box-shadow: 0px 0px 2px rgba(0,0,0,0.5);
+                border-radius: 1rem;
+                background-color: #ffd701;
+            }
+            .card-logo{
+                display: flex;
+                justify-content: center;
+                border-top-left-radius: 1rem;
+                border-top-right-radius: 1rem;
+                background-color: #ffd701;
+            }
+            .card-head{
+                background-color: #ffd701;
+                padding: 0.5rem 1rem;
+            }
+            .card-title{
+                font-size: 1.4rem;
+            }
+            .card-body{
+                display: flex;
+                flex-direction: column;
+            }
+            .card-body-section{
+                padding: 0.5rem 1rem;
+        
+            }
+            .tag-container{
+                display: flex;
+                align-items: center;
+                gap: 10px;
+            }
+            .tag-title{
+                font-size: 1rem;
+            }
+            .tag-text{
+                font-size: 0.8rem;
+            }   
+            
+        
+        </style>
+        <body>
+            
+            <div class="card">
+                <div class="card-logo">
+                    <img src="https://i.pinimg.com/originals/66/be/9c/66be9cd6d577011b040bab27112bfb59.jpg" width="100" height="100">
+                </div>
+                <div class="card-head">
+                    <h1 class="card-title">Muchas Gracias por realizar el pedido, para confirmarlo porfavor responder a este correo</h1>
+                </div>
+                <div class="card-body">
+                    <div class="card-body-section">
+                        <div class="tag-container">
+                            <h2 class="tag-title">Nombre:</h2>
+                            <p class="tag-text">${orderData.nombre}</p>
+                        </div>
+                    </div>
+                    <div class="card-body-section">
+                        <div class="tag-container">
+                            <h2 class="tag-title">Cedula:</h2>
+                            <p class="tag-text">${orderData.cedula}</p>
+                        </div>
+                    </div>
+                    <div class="card-body-section">
+                        <div class="tag-container">
+                            <h2 class="tag-title">Direccion:</h2>
+                            <p class="tag-text">${orderData.direccion}</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="card-body-section">
+                        <h2 class="tag-title">Descripcion del Pedido</h2>
+                        <p class="tag-text">${orderData.pedido}</p>
+                    </div>
+                </div>
+                <div class="card-foot"></div>
             </div>
-            <div style="background-color:#05C7F2; margin:0; border-bottom-left-radius: 0.4rem; border-bottom-right-radius: 0.4rem;">
-                <h3 style="text-align:center; font-size:1rem; padding:1rem;">Los Pollos Hermanos</h3>
-            </div>
-        </div>
+        
+        </body>
         `
     }
 
